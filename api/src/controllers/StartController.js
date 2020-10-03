67ec6e65ecb31272b659e1f5178d6451d1a5047b
Buzzi <buzzi.giovanni@outlook.com>
2020-10-03 15:52:24 -0300

    Adicionado dados default da maria (como pedindo no desafio)

--- api/src/controllers/StartController.js
+++ api/src/controllers/StartController.js
@@ -12,16 +12,16 @@ module.exports = {
         await Team.create({name:'HEAT'});
         await Team.create({name:'BUCKS'});
         
-        await User.create({name:'Maria', login:'maria', password:'maria', id_team:1, max_season:100, min_season:15, max_record:1, min_record: 3});
+        await User.create({name:'Maria', login:'maria', password:'maria', id_team:1, max_season:24, min_season:10, max_record:1, min_record: 1});
         await User.create({name:'Fabio', login:'fabio', password:'fabio', id_team:2, max_season:100, min_season:15, max_record:1, min_record: 3});
         await User.create({name:'Jordan', login:'jordan', password:'jordan', id_team:3, max_season:100, min_season:15, max_record:1, min_record: 3});
         await User.create({name:'Rose', login:'rose', password:'rose', id_team:4, max_season:100, min_season:15, max_record:1, min_record: 3});
         await User.create({name:'Harden', login:'harden', password:'harden', id_team:5, max_season:100, min_season:15, max_record:1, min_record: 3});
 
-        await Match.create({points:100, id_adversary:2, id_user:1});
-        await Match.create({points:20, id_adversary:3, id_user:1});
-        await Match.create({points:40, id_adversary:4, id_user:1});
-        await Match.create({points:15, id_adversary:5, id_user:1});
+        await Match.create({points:12, id_adversary:2, id_user:1});
+        await Match.create({points:24, id_adversary:3, id_user:1});
+        await Match.create({points:10, id_adversary:4, id_user:1});
+        await Match.create({points:24, id_adversary:5, id_user:1});
 
         await Match.create({points:100, id_adversary:3, id_user:2});
         await Match.create({points:20, id_adversary:4, id_user:2});
